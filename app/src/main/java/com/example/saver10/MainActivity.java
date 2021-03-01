e0a758f74affea67122d65c72424ace43a886d52
asmezas <79260410+asmezas@users.noreply.github.com>
2021-03-01 20:52:48 +0200

    Tarleva

--- app/src/main/java/com/example/saver10/MainActivity.java
+++ app/src/main/java/com/example/saver10/MainActivity.java
@@ -18,23 +18,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        int x = 1;
-        Spinner MySpinner = (Spinner) findViewById(R.id.spinner);
-        ArrayAdapter<String> myAdapter = new ArrayAdapter<String>(MainActivity.this,
-                android.R.layout.simple_expandable_list_item_1, getResources().getStringArray(R.array.options));
-        myAdapter.setDropDownViewResource(android.R.layout.simple_dropdown_item_1line);
-        MySpinner.setAdapter(myAdapter);
-        onOptionsItemSelected(MySpinner);
-
     }
-    public boolean onOptionsItemSelected (Spinner spinner)
-    {
-        String selected = spinner.getSelectedItem().toString();
-        if (selected.equals("Senas"))
-        {
-                setContentView(R.layout.activity_menesiu);
-        }
 
-        return true;
-    }
 }
--- app/src/main/java/com/example/saver10/MenesiuActivity.java
+++ app/src/main/java/com/example/saver10/MenesiuActivity.java
@@ -3,6 +3,11 @@ package com.example.saver10;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 public class MenesiuActivity extends AppCompatActivity {
 
@@ -10,5 +15,22 @@ public class MenesiuActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menesiu);
+        Spinner MySpinner = (Spinner) findViewById(R.id.Pasirinkimai_Spinners);
+        ArrayAdapter<String> myAdapter = new ArrayAdapter<String>(MenesiuActivity.this,
+                android.R.layout.simple_expandable_list_item_1, getResources().getStringArray(R.array.options));
+        myAdapter.setDropDownViewResource(android.R.layout.simple_dropdown_item_1line);
+        MySpinner.setAdapter(myAdapter);
+        TextView naujas = (TextView) findViewById(R.id.Tekstas_Koks_Menuo);
+
+        Button buttonofpasirinkimai = findViewById(R.id.Button_Pasirinkti);
+       buttonofpasirinkimai.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+               if( MySpinner.getSelectedItem().toString().equals("January"))
+                {
+                    naujas.setText(MySpinner.getSelectedItem().toString());
+                }
+            }
+        });
     }
 }
