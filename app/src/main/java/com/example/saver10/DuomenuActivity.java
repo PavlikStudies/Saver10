cc10d94058e5f07a3df3fb1927de357b550bf86e
asmezas <79260410+asmezas@users.noreply.github.com>
2021-04-21 21:25:15 +0300

    Difrrent manifest

--- app/src/main/java/com/example/saver10/DuomenuActivity.java
+++ app/src/main/java/com/example/saver10/DuomenuActivity.java
@@ -4,14 +4,55 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.jar.Attributes;
 
 public class DuomenuActivity extends AppCompatActivity {
 
+    InputStream is;
+    String lapatai="tuscia";
+    ArrayList<MountData> menesio = new ArrayList<MountData>();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_duomenu);
         TextView labas = (TextView) findViewById(R.id.First_Atribute);
-        labas.setText("Atejau");
+        try {
+            is = getApplicationContext().getAssets().open("bla.txt");
+        } catch (IOException e) {
+            Toast.makeText(getApplicationContext(),"Problems: " + e.getMessage(), Toast.LENGTH_LONG).show();
+        }
+        BufferedReader br = new BufferedReader(new InputStreamReader(is, StandardCharsets.UTF_8));
+        menesio = GetMounthData(br);
+        labas.setText(menesio.get(0).Label);
+    }
+    public ArrayList<MountData> GetMounthData(BufferedReader br)
+    {
+        ArrayList<MountData> data = new ArrayList<MountData>();
+        try {
+            while ((lapatai = br.readLine()) != null)
+            {
+                String[] Lines = lapatai.split(";");
+                for (int i = 0; i < Lines.length; i++)
+                {
+                    String[] parts = Lines[i].split(" ");
+                    String Name = (parts[0]);
+                    double NumberOfMarks = Double.parseDouble(parts[1]);
+                    MountData S = new MountData(Name,NumberOfMarks);
+                    data.add(S);
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return data;
     }
 }
--- app/src/main/java/com/example/saver10/MountData.java
+++ app/src/main/java/com/example/saver10/MountData.java
@@ -2,13 +2,15 @@ package com.example.saver10;
 
 public class MountData {
     public String Label ;
-    public int Value;
-    public String GetDataName() { return Label;}
-    public double GetValue() {return Value;}
+    public double Value;
 
-    public MountData(String Name,int number)
+
+    public MountData(String Name,double number)
     {
         Label =Name;
         Value = number;
     }
+
+    public String GetDataName() { return Label;}
+    public double GetValue() {return Value;}
 }
