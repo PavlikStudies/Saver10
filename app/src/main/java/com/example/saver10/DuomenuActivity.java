03af4b4fc1aeb84c9f3ff5f91e17f33abf70032b
asmezas <79260410+asmezas@users.noreply.github.com>
2021-04-28 16:40:11 +0300

    FILE READING AND WRITING

--- app/src/main/java/com/example/saver10/DuomenuActivity.java
+++ app/src/main/java/com/example/saver10/DuomenuActivity.java
@@ -15,6 +15,7 @@ import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.Console;
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.FileReader;
 import java.io.FileWriter;
@@ -25,30 +26,35 @@ import java.io.Writer;
 import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 
-public class DuomenuActivity extends AppCompatActivity {
+import static android.widget.Toast.*;
 
-    InputStream is;
-    String lapatai="tuscia";
-    ArrayList<MountData> menesio = new ArrayList<MountData>();
+public class DuomenuActivity extends AppCompatActivity {
+    String lapatai="";
+    ArrayList<MountData> MASYVAS = new ArrayList<MountData>();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_duomenu);
-        TextView labas = (TextView) findViewById(R.id.First_Atribute);
-        TextView labas2 = (TextView) findViewById(R.id.first_Atribute2);
-        try {
-            is = getApplicationContext().getAssets().open("bla.txt");
-        } catch (IOException e) {
-            Toast.makeText(getApplicationContext(),"Problems: " + e.getMessage(), Toast.LENGTH_LONG).show();
-        }
-        BufferedReader br = new BufferedReader(new InputStreamReader(is, StandardCharsets.UTF_8));
-        menesio = GetMounthData(br);
-        labas.setText(menesio.get(0).Label);
-        menesio.get(0).SetValue(2);
-        RewriteData(menesio,is);
+       TextView labas = (TextView) findViewById(R.id.textView);
+        /*TextView labas2 = (TextView) findViewById(R.id.first_Atribute2);*/
+        String filename = "Labas.txt";
+        String filepath = "MyfileDirectory";
+        File myExternalFile = new File(getExternalFilesDir(filepath),filename);
+        RewriteData(MASYVAS,myExternalFile);
+        MASYVAS = GetMounthData(filename,filepath,myExternalFile);
+        labas.setText(MASYVAS.get(0).Label+MASYVAS.get(0).Value + MASYVAS.get(1).Label + MASYVAS.get(1).Value);
     }
-    public ArrayList<MountData> GetMounthData(BufferedReader br)
+
+    public ArrayList<MountData> GetMounthData(String filepath,String filename,File PaimtasFailas)
     {
+        FileReader fr =null;
+        try {
+            fr = new FileReader(PaimtasFailas);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        BufferedReader br = new BufferedReader(fr);
         ArrayList<MountData> data = new ArrayList<MountData>();
         try {
             while ((lapatai = br.readLine()) != null)
@@ -62,48 +68,34 @@ public class DuomenuActivity extends AppCompatActivity {
                     MountData S = new MountData(Name,NumberOfMarks);
                     data.add(S);
                 }
-
             }
+            br.close();
         } catch (IOException e) {
             e.printStackTrace();
         }
         return data;
     }
 
-    public void RewriteData (ArrayList<MountData> data, InputStream la)
+    public void RewriteData (ArrayList<MountData> data, File Paimtasfailas)
     {
-        /*FileOutputStream outputStream = new FileOutputStream(getApplicationContext().getFileStreamPath("ba.txt"));*/
-            File file = new File(DuomenuActivity.this.getFilesDir(), "text");
-            if (!file.exists()) {
-                file.mkdir();
-            }
-            try {
-                File gpxfile = new File(file, "sample");
-                FileWriter writer = new FileWriter(gpxfile);
-                writer.append("Labas");
-                writer.flush();
-                writer.close();
-                Toast.makeText(DuomenuActivity.this, "Saved your text", Toast.LENGTH_LONG).show();
-            } catch (Exception e) { }
-
- /*       String path = "lalaa";
-        Log.i("lala",path);
-        File myDir = new File("labas.txt");
-        try {
-            myDir.createNewFile();
+        FileOutputStream fos = null;
+        try{
+            String text = "January 30;";
+            String text1 ="September 50;";
+            String fileContent = text.trim();
+            /// RASYMAS SU FILE OUTPUTSTREAM
+            /*fos = new FileOutputStream(Paimtasfailas,true);
+            fos.write(fileContent.getBytes());
+            fos.close();
+             */
+            FileWriter Writer = new FileWriter(Paimtasfailas);
+            Writer.write(text);
+            Writer.write(text1);
+            Writer.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
         } catch (IOException e) {
             e.printStackTrace();
-            Log.i("lala","Nepaejo");
         }
-*/
-        /*BufferedWriter bw = new BufferedWriter();
-        for(int i=0; i<data.size(); i++) {
-            try {
-                bw.write(data.get(i).Label + " " + data.get(i).Value + ";");
-
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }*/
     }
 }
